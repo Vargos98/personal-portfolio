f5bcacba5fa8894efa968f9cfcd1a70ee000a1af
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-08-21 10:46:23 +0530

    Added react-icons and additional files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 
 
-export default function App() {
+function App() {
   return (
-    <div className='text-4xl font-bold'>Hello</div>
-  )
+    <div>
+      <h1 className="text-4xl font-bold">Hello</h1>
+    </div>
+  );
 }
+
+export default App;
