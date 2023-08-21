afff8cc455354cfa66aa15172600366614912aa7
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-08-21 12:06:33 +0530

    Navbar completed and working on Homepage

---
+++ src/components/Home.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import HeroImage from '../assets/heroImage.png';
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
+const Home = () => {
+    return (
+        <div name="home" className='h-screen w-full bg-gradient-to-b from-black to-gray-800'>
+            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+                <div className='flex flex-col justify-center h-full'>
+                    <h2 className='text-4xl sm:text-7xl font-bold text-white'>I am a Frontend Developer !</h2>
+                    <p className='text-gray-500 py-4 max-w-md'>
+                        I my thirst for learning web development has
+                        no bound, currently I am learning new frameworks and
+                        technologies in Web Dev in-order to excel and grow.
+                        My main focus is on learning and would love to work in place where learning aspects are abundant !
+                    </p>
+                    <div>
+                        <button>
+                            Portfolio
+                            <span>
+                                <MdOutlineKeyboardArrowRight />
+                            </span>
+
+                        </button>
+                    </div>
+                </div>
+                <div>
+                    <img src={HeroImage} alt="profilepic" 
+                    className='rounded-2xl mx-auto w-2/3 md:w-full'/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
