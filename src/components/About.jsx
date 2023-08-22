05c2a8e815a08476c18cc04ace5e98dbe3e3ab88
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-08-22 21:14:29 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,22 +8,22 @@ const About = () => {
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-      
+
           <p className="text-4xl bold inline border-b-4 border-gray-500">
             About
           </p>
         </div>
 
-        
+
 
         <br />
 
         <p className="text-xl">
-          I am currently based in Rajsthan,India and I have completed my Bachelors in Computer Science and Technology in 2021 from JK Lakshmipat University, Jaipur, Rajasthan. 
+          I am currently based in Jaipur, Rajasthan and I have completed my Bachelors in Computer Science and Technology in 2021 from JK Lakshmipat University Jaipur, Rajasthan.
           I have a passion for building web applications and designing beautiful user interfaces.
-          Recently I've been working with technologies like Javascript, React, Tailwind and I'm currently learning Node.js and other backend frameworks.
+          Recently, I've been working with technologies like JavaScript, React.js, and Tailwind, and I'm currently learning Node.js and other backend technologies.
         </p>
-    
+
       </div>
     </div>
   );
