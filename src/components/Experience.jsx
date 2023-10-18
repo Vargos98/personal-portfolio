5202d980803dcf1ea89813f08a300b0eeb49bd67
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-10-18 10:41:07 +0530

    Added transitions in navbar and homepage

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -101,7 +101,7 @@ const Experience = () => {
               key={id}
               className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
             >
-              <img src={src} alt="" className="w-20 mx-auto" />
+              <img src={src} alt="" className="w-20 mx-auto hover:scale-110 duration-300" />
               <p className="mt-4">{title}</p>
             </div>
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
-        <h1 className="text-2xl  ml-2 font">Umesh Kumar</h1>
+        <h1 className="text-2xl  ml-2 font hover:scale-110 duration-300">Umesh Kumar</h1>
       </div>
 
       <ul className="hidden md:flex">
