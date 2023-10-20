bf14f1567406d2026c9a7dd647f760c853a1ff07
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-10-20 18:30:19 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
           <p className="text-4xl bold inline border-b-4 border-gray-500 ">
             Contact
           </p>
-          <p className="py-6">Submit the form below to get in touch with me</p>
+          <p className="py-6">Wanna chat! hit me up, I'm ready to help <span className="text-4xl">😉</span></p>
         </div>
 
         <div className="flex justify-center items-center">
