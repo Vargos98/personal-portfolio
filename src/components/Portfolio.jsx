f49ff13957cf381581f7ac3278515b901d750afe
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-01-04 11:20:39 +0530

    Added Express, mongoDB, vercel and Next js in Experience section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,7 +4,8 @@ import GridNotes from '../assets/portfolio/GridNotes.png';
 import MyNotes from '../assets/portfolio/MyNotes.png';
 import ItunesClone from '../assets/portfolio/ItunesClone.png';
 import TheChayaChitra from '../assets/portfolio/TheChayaChitra.png';
-import TinderDogs from '../assets/portfolio/TinderDogs.png';
+// import TinderDogs from '../assets/portfolio/TinderDogs.png';
+import reactEssentials from '../assets/portfolio/React-Essentials.png';
 
 const Portfolio = () => {
 
@@ -31,6 +32,15 @@ const Portfolio = () => {
                         </div>
                         <p className='text-md bold inline ml-5 mt-5 mb-5 font-bold'>Tech Stack </p>
                         <p className='mt-5 mb-5 flex items-center justify-center '> React.js</p>
+                    </div>
+                    <div className='shadow-md shadow-gray-600 rounded-r-lg duration-200 hover:scale-110'>
+                        <img src={reactEssentials} alt="" className='rounded-md duration-200 hover:scale-105' />
+                        <div className='flex items-center justify-center'>
+                            <a href="https://react-essentials-rosy.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</a>
+                            <a href="https://github.com/Vargos98/react-essentials" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</a>
+                        </div>
+                        <p className='text-md bold inline ml-5 mt-5 mb-5 font-bold'>Tech Stack </p>
+                        <p className='mt-5 mb-5 flex items-center justify-center '>React.js</p>
                     </div>
 
                     <div className='shadow-md shadow-gray-600 rounded-r-lg duration-200 hover:scale-110'>
@@ -60,18 +70,10 @@ const Portfolio = () => {
                             <a href="https://github.com/Vargos98/grid-notes" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</a>
                         </div>
                         <p className='text-md bold inline ml-5 mt-5 mb-5 font-bold'>Tech Stack </p>
-                        <p className='mt-5 mb-5 flex items-center justify-center'> React.js, CSS, HTML</p>
+                        <p className='mt-5 mb-5 flex items-center justify-center'> React.js</p>
                     </div>
 
-                    <div className='shadow-md shadow-gray-600 rounded-r-lg duration-200 hover:scale-110'>
-                        <img src={TinderDogs} alt="" className='rounded-md duration-200 hover:scale-105' />
-                        <div className='flex items-center justify-center'>
-                            <a href="https://tin-dog-phi.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</a>
-                            <a href="https://github.com/Vargos98/TinDog" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</a>
-                        </div>
-                        <p className='text-md bold inline ml-5 mt-5 mb-5 font-bold'>Tech Stack </p>
-                        <p className='mt-5 mb-5 flex items-center justify-center '>HTML, Bootstrap</p>
-                    </div>
+                   
 
                     <div className='shadow-md shadow-gray-600 rounded-r-lg duration-200 hover:scale-110'>
                         <img src={MyNotes} alt="" className='rounded-md duration-200 hover:scale-105' />
