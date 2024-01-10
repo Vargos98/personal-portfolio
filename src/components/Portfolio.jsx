77f5a9a2189f73553e4d941edf83d0e4e812dc60
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-01-10 13:35:47 +0530

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -29,9 +29,9 @@ const Portfolio = () => {
                             <a href="https://github.com/Vargos98/restaurant-react-app" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</a>
 
                         </div>
-                        <p className='text-md bold inline ml-5 mt-5 mb-5 font-bold'>Tech Stack : React.js</p>
-                       
-                        <p className='mt-2 mb-5 ml-3 flex items-start fs-2 '>Fiesta La Blanc is a React.js project based upon a restaurant website. Also, having emersive layout making it usercentric.</p>
+                        <p className='text-md bold inline ml-4 mt-5 mb-5 font-bold text-cyan-400'>Tech Stack : <span className=' text-xs mt-2 text-red-300 ml-1 '> React.js</span> </p>
+                        <p ></p>
+                        <p className='mt-2 mb-5 ml-4 flex items-start fs-2 '>Fiesta La Blanc is a React.js project based upon a restaurant website. Also, having emersive layout making it usercentric.</p>
                     </div>
                     <div className='shadow-md shadow-gray-600 rounded-r-lg duration-200 hover:scale-110'>
                         <img src={reactEssentials} alt="" className='rounded-md duration-200 hover:scale-105' />
@@ -39,8 +39,9 @@ const Portfolio = () => {
                             <a href="https://react-essentials-rosy.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</a>
                             <a href="https://github.com/Vargos98/react-essentials" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</a>
                         </div>
-                        <p className='text-md bold inline ml-5 mt-5 mb-5 font-bold'>Tech Stack : React.js</p>
-                        <p className='mt-2 mb-5 ml-3 flex items-start fs-2 '>Project built for clearing basics for react.js and its fundamentals. Having several code snippets based on different methods.</p>
+                        <p className='text-md bold inline ml-4 mt-5 mb-5 font-bold text-cyan-400'>Tech Stack : <span className=' text-xs mt-2 text-red-300 ml-1 '> React.js</span></p>
+
+                        <p className='mt-2 mb-5 ml-4 flex items-start fs-2 '>Project built for clearing basics for react.js and its fundamentals. Having several code snippets based on different methods.</p>
                         
                     </div>
 
@@ -50,8 +51,9 @@ const Portfolio = () => {
                             <a href="https://itunes-clone.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</a>
                             <a href="https://github.com/Vargos98/Itunes-clone" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</a>
                         </div>
-                        <p className='text-md bold inline ml-5 mt-5 mb-5 font-bold'>Tech Stack : HTML, CSS, JQuery</p>
-                        <p className='mt-2 mb-5 ml-3 flex items-start fs-2 '> Cloned & customized a web project inspired by iTunes, showcasing strong front-end development skills and user interface design expertise.</p>
+                        <p className='text-md bold inline ml-4 mt-5 mb-5 font-bold text-cyan-400'>Tech Stack :<span className=' text-xs mt-2 text-red-300 ml-1 '>HTML, CSS, JQuery</span> </p>
+                    
+                        <p className='mt-2 mb-5 ml-4 flex items-start fs-2 '> Cloned & customized a web project inspired by iTunes, showcasing strong front-end development skills and user interface design expertise.</p>
                        
                     </div>
 
@@ -61,8 +63,9 @@ const Portfolio = () => {
                             <a href="https://the-chaya-chitra.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</a>
                             <a href="https://github.com/Vargos98/The-Chaya-Chitra" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</a>
                         </div>
-                        <p className='text-md bold inline ml-5 mt-5 mb-5 font-bold'>Tech Stack : HTML, CSS, JQuery</p>
-                        <p className='mt-2 mb-5 ml-3 flex items-start fs-2 '>Website designed to showcase services provided by the company based on multi-level photography and videography.</p>
+                        <p className='text-md bold inline ml-4 mt-5 mb-5 font-bold text-cyan-400'>Tech Stack :<span className=' text-xs mt-2 text-red-300 ml-1 '>HTML, CSS, JQuery</span></p>
+                  
+                        <p className='mt-2 mb-5 ml-4 flex items-start fs-2 '>Website designed to showcase services provided by the company based on multi-level photography and videography.</p>
                     </div>
 
                     <div className='shadow-md shadow-gray-600 rounded-r-lg duration-200 hover:scale-110'>
@@ -71,8 +74,9 @@ const Portfolio = () => {
                             <a href="https://grid-notes.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</a>
                             <a href="https://github.com/Vargos98/grid-notes" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</a>
                         </div>
-                        <p className='text-md bold inline ml-5 mt-5 mb-5 font-bold'>Tech Stack : React.js</p>
-                        <p className='mt-2 mb-5 ml-3 flex items-start fs-2 '>Designed & developed a user-friendly & mobile-friendly note-taking application using React, implemented a responsive grid layout for organizing and visualizing notes.</p>
+                        <p className='text-md bold inline ml-4 mt-5 mb-5 font-bold text-cyan-400'>Tech Stack : <span className=' text-xs mt-2 text-red-300 ml-1 '> React.js</span> </p>
+                    
+                        <p className='mt-2 mb-5 ml-4 flex items-start fs-2 text-wrap '>Designed & developed a user-friendly & mobile-friendly note-taking application using React, implemented a responsive grid layout for organizing and visualizing notes.</p>
                     </div>
 
                    
@@ -83,8 +87,9 @@ const Portfolio = () => {
                             <a href="https://mynotes98.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</a>
                             <a href="https://github.com/Vargos98/mynotes98" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</a>
                         </div>
-                        <p className='text-md bold inline ml-5 mt-5 mb-5 font-bold'>Tech Stack : React.js </p>
-                        <p className='mt-2 mb-5 ml-3 flex items-start fs-2 '>Another Note making app designed to store data locally on device. So, the user could close/reload the page and does not loose its feeds.</p>
+                        <p className='text-md bold inline ml-4 mt-5 mb-5 font-bold text-cyan-400'>Tech Stack : <span className=' text-xs mt-2 text-red-300 ml-1 '> React.js</span> </p>
+
+                        <p className='mt-2 mb-5 ml-4 flex items-start fs-2 text-wrap '>Another Note making app designed to store data locally on device. So, the user could close/reload the page and does not loose its feeds.</p>
                     </div>
                     <span></span>
                     <span></span>
