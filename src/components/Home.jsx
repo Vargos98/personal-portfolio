d192d013265e420e0e76f35621dbd2434254b37b
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-05-03 20:43:31 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -79,7 +79,7 @@ const Home = () => {
                     <div className='text-white  mt-2 mr-1'>
                         <div>
                             <form method="get" action="/UmeshKumar_Resume.pdf">
-                                <button class="bg-gradient-to-r from-green-400 to-blue-500 text-gray-800 font-bold duration-200 hover:scale-110 py-2 px-4 rounded inline-flex items-center ">
+                                <button class="bg-gradient-to-r from-white to-white text-gray-800 font-bold duration-200 hover:scale-110 py-2 px-4 rounded inline-flex items-center ">
                                     <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z "  /></svg>
                                     <span>Download Resume</span>
                                 </button>
