54b326621957d4b9398629dac74bcc8ccfd57ecb
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-07-01 12:33:41 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,18 +11,18 @@ const About = () => {
 
           <p className="text-4xl bold inline border-b-4 border-yellow-500 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500
         inline-block text-transparent bg-clip-text">
-            About
+           <strong> About</strong> 
           </p>
         </div>
-        <p className="text-xl">
-          I am a web developer out in Jaipur and I have completed my Bachelors in Computer Science and Technology from JK Lakshmipat University Jaipur.
+        <p className="text-xl ">
+          I am a web developer out in Jaipur and I have completed my <strong> Bachelors in Computer Science and Technology from JK Lakshmipat University Jaipur. </strong> 
           I have a passion for building web applications and designing beautiful user interfaces.<br/>
           Technology excites me and I am always in awe of the change it drives in the world.<br/> 
-          Certain skills that I have worked with include Data Structures and Algorithms (Java, C++),<br/> 
-          Web Development (MERN Stack), and what I might lack in 
-          skills I make up for with my determination to learn.<br/>
+          Certain skills that I have worked with include Data Structures and Algorithms <strong>(Java, C++)</strong>,<br/> 
+          Web Development <strong>(MERN Stack)</strong>, and what I might lack in 
+          skills I make up for with my determination to learn.
           Outside of tech, I am a coffee lover, and loves to participate in numerous sports   
-          in which swimming and badminton are my favourites.<br/> Beside that I love cooking, 
+          in which swimming and badminton are my favourites. Beside that I love cooking, 
           as cooking is simple therapeutic. It does wonders
           in terms of unwinding and relaxing.
         </p>
