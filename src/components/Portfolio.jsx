6ef3ce2a6ab3b79d350d278efda3b12347842a0f
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-07-01 12:40:51 +0530

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -30,8 +30,8 @@ const Portfolio = () => {
                     <div className='shadow-md shadow-gray-600 rounded-l-xl rounded-r-xl  duration-200 hover:scale-110'>
                         <img src={FiestaLaBlanc} alt="" className='rounded-xl duration-200 hover:scale-105' />
                         <div className='flex items-center justify-center '>
-                            <a href="https://restaurant-react-app-rust.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'>Demo</a>
-                            <a href="https://github.com/Vargos98/restaurant-react-app" className='w-1 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'>Code</a>
+                            <a href="https://restaurant-react-app-rust.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'><strong>Demo</strong></a>
+                            <a href="https://github.com/Vargos98/restaurant-react-app" className='w-1 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'><strong>Code</strong></a>
 
                         </div>
                         <p className='text-md bold inline ml-4 mt-5 mb-5 font-bold text-cyan-400'>Tech Stack : <span className=' text-xs mt-2 text-red-300 ml-1 '> React.js</span> </p>
@@ -45,8 +45,8 @@ const Portfolio = () => {
                     <div className='shadow-md shadow-gray-600 rounded-l-xl rounded-r-xl duration-200 hover:scale-110'>
                         <img src={reactEssentials} alt="" className='rounded-xl  duration-200 hover:scale-105' />
                         <div className='flex items-center justify-center'>
-                            <a href="https://react-essentials-rosy.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'>Demo</a>
-                            <a href="https://github.com/Vargos98/react-essentials" className='w-1 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'>Code</a>
+                            <a href="https://react-essentials-rosy.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'><strong>Demo</strong></a>
+                            <a href="https://github.com/Vargos98/react-essentials" className='w-1 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'><strong>Code</strong></a>
                         </div>
                         <p className='text-md bold inline ml-4 mt-5 mb-5 font-bold text-cyan-400'>Tech Stack : <span className=' text-xs mt-2 text-red-300 ml-1 '> React.js</span></p>
 
@@ -58,8 +58,8 @@ const Portfolio = () => {
                     <div className='shadow-md shadow-gray-600 rounded-l-xl rounded-r-xl  duration-200 hover:scale-110'>
                         <img src={fashion} alt="" className='rounded-xl  duration-200 hover:scale-105' />
                         <div className='flex items-center justify-center'>
-                            <a href="https://dribble-fashion-week.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'>Demo</a>
-                            <a href="https://github.com/Vargos98/profile-page/tree/main" className='w-1 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'>Code</a>
+                            <a href="https://dribble-fashion-week.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'><strong>Demo</strong></a>
+                            <a href="https://github.com/Vargos98/profile-page/tree/main" className='w-1 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'><strong>Code</strong></a>
                         </div>
                         <p className='text-md bold inline ml-4 mt-5 mb-5 font-bold text-cyan-400'>Tech Stack :<span className=' text-xs mt-2 text-red-300 ml-1 '>HTML, CSS, GRID-Layout</span> </p>
 
@@ -71,8 +71,8 @@ const Portfolio = () => {
                     <div className='shadow-md shadow-gray-600 rounded-l-xl rounded-r-xl duration-200 hover:scale-110'>
                         <img src={premier} alt="" className='rounded-xl  duration-200 hover:scale-105' />
                         <div className='flex items-center justify-center'>
-                            <a href="https://premier-website-beta.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'>Demo</a>
-                            <a href="https://github.com/Vargos98/Premier-website" className='w-1 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'>Code</a>
+                            <a href="https://premier-website-beta.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'><strong>Demo</strong></a>
+                            <a href="https://github.com/Vargos98/Premier-website" className='w-1 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'><strong>Code</strong></a>
                         </div>
                         <p className='text-md bold inline ml-4 mt-5 mb-5 font-bold text-cyan-400'>Tech Stack : <span className=' text-xs mt-2 text-red-300 ml-1 '> HTML, CSS, GRID-Layout</span> </p>
 
@@ -83,8 +83,8 @@ const Portfolio = () => {
                     <div className='shadow-md shadow-gray-600 rounded-l-xl rounded-r-xl duration-200 hover:scale-110'>
                         <img src={trendline} alt="" className='rounded-xl  duration-200 hover:scale-105' />
                         <div className='flex items-center justify-center'>
-                            <a href="https://trendline-dribble.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'>Demo</a>
-                            <a href="https://github.com/Vargos98/Trendline-dribble" className='w-1 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'>Code</a>
+                            <a href="https://trendline-dribble.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'><strong>Demo</strong></a>
+                            <a href="https://github.com/Vargos98/Trendline-dribble" className='w-1 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'><strong>Code</strong></a>
                         </div>
                         <p className='text-md bold inline ml-4 mt-5 mb-5 font-bold text-cyan-400'>Tech Stack :<span className=' text-xs mt-2 text-red-300 ml-1 '>HTML, CSS, Flexbox</span></p>
 
@@ -95,8 +95,8 @@ const Portfolio = () => {
                     <div className='shadow-md shadow-gray-600 rounded-l-xl rounded-r-xl   duration-200 hover:scale-110'>
                         <img src={TheChayaChitra} alt="" className='rounded-xl duration-200 hover:scale-105' />
                         <div className='flex items-center justify-center'>
-                            <a href="https://the-chaya-chitra.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'>Demo</a>
-                            <a href="https://github.com/Vargos98/The-Chaya-Chitra" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'>Code</a>
+                            <a href="https://the-chaya-chitra.vercel.app/" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'><strong>Demo</strong></a>
+                            <a href="https://github.com/Vargos98/The-Chaya-Chitra" className='w-1 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400'><strong>Code</strong></a>
                         </div>
                         <p className='text-md bold inline ml-4 mt-5 mb-5 font-bold text-cyan-400'>Tech Stack :<span className=' text-xs mt-2 text-red-300 ml-1 '>HTML, CSS, JQuery</span></p>
 
