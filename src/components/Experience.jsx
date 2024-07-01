aab8331f1ed4c38a1730e99e2c3c5b4f7b71d9cc
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-07-01 12:59:33 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -110,14 +110,14 @@ const Experience = () => {
           <p className="py-8">These are the technologies I've worked with</p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-5 gap-8 text-center py-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-5 gap-8 text-center py-8 px-12 sm:px-0 ">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
-              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+              className={`shadow-md hover:scale-105 py-6 rounded-full hover:shadow-lg duration-500 ${style}`}
             >
-              <img src={src} alt="" className="w-20 mx-auto hover:scale-110 duration-300" />
-              <p className="mt-4">{title}</p>
+              <img src={src} alt="" className="w-20  mx-auto hover:scale-110 duration-300 rounded-full" />
+              <p className="mt-4 hover:font-bold">{title}</p>
             </div>
 
           ))}
